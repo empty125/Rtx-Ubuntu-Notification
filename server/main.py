87856ca7e57fddf93ac5c6a9494ad9337ed6401a
xilei <xilei125@163.com>
2015-05-04 18:28:39 +0800

    添加消息时间

--- server/main.py
+++ server/main.py
@@ -2,21 +2,23 @@
 
 import socketserver
 import notify
+from datetime import datetime
 
 class NotificationUDPHandler(socketserver.BaseRequestHandler):
 
 	def handle(self):
 		data = self.request[0].decode('utf-8').strip()
 		socket = self.request[1]
-		print("{0} wrote:{1}".format(self.client_address[0],data))
-		notify.show(data)
+		now_time = datetime.now().strftime("%H:%M:%S")
+		print("{0} {1} wrote:{2}".format(
+			now_time,
+			self.client_address[0],			
+			data
+			))
+		notify.show(now_time+' '+data)
 		socket.sendto("ok \n".encode('utf-8'),self.client_address)
 
 
 if __name__ =="__main__":
-	# Create the server, binding to localhost on port 9999
 	server = socketserver.UDPServer(("192.168.122.1", 10086), NotificationUDPHandler)
-
-	# Activate the server; this will keep running until you
-	# interrupt the program with Ctrl-C
 	server.serve_forever()
